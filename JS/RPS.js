c9b19bfd7eaef9db7b69059d6cd9b49ea46157bd
Luca graves <lgrave264@west-mec.org>
2020-12-07 17:46:02 -0700

    finished

--- JS/RPS.js
+++ JS/RPS.js
@@ -1,6 +1,16 @@
 var rock = 0; 
 var paper = 0;
 var scissors = 0;
+var Airock = 0; 
+var Aipaper = 0;
+var Aiscissors = 0;
+var win = 0;
+var tie = 0;
+var lose = 0;
+
+document.getElementById('win').value = win;
+document.getElementById('tie').value = tie;
+document.getElementById('lose').value = lose;
 
 function crock(){
     rock = 1; 
@@ -30,14 +40,76 @@ function cscissors(){
 
 
 function RPS(){
-    let random = (Math.random() * 3) + 1;
+    var random = Math.floor(Math.random() * 3) + 1;
     if (random == 1){
-        rock = 1;
+        Airock = 1;
+        Aipaper = 0;
+        Aiscissors = 0;
     }
     if(random == 2){
-        paper = 1;
+        Airock = 0;
+        Aipaper = 1;
+        Aiscissors = 0;
     }
     if(random == 3){
-        scissors = 1;
+        Airock = 0;
+        Aipaper = 0;
+        Aiscissors = 1;
+    }
+    if(rock == 1 && Aiscissors == 1){
+        //win
+        console.log("win");
+        document.getElementById("status").innerHTML = "win";
+        win += 1;
+        document.getElementById('win').innerHTML = win;
+    }else if(rock == 1 && Airock == 1){
+        //tie
+        console.log("tie");
+        document.getElementById("status").innerHTML = "tie";
+        tie += 1;
+        document.getElementById('tie').innerHTML = tie;
+    }else if (rock == 1 && Aipaper == 1){
+        //lose
+        console.log("lose");
+        document.getElementById("status").innerHTML = "lose";
+        lose += 1;
+        document.getElementById('lose').innerHTML = lose;
+    }else if(paper == 1 && Airock == 1){
+        //win
+        console.log("win");
+        document.getElementById("status").innerHTML = "win";
+        win += 1;
+        document.getElementById('win').innerHTML = win;
+    }else if(paper == 1 && Aipaper == 1){
+        //tie
+        console.log("tie");
+        document.getElementById("status").innerHTML = "tie";
+        tie += 1;
+        document.getElementById('tie').innerHTML = tie;
+    }else if(paper == 1 && Aiscissors == 1){
+        //lose
+        console.log("lose");
+        document.getElementById("status").innerHTML = "lose";
+        lose += 1;
+        document.getElementById('lose').innerHTML = lose;
+    }else if(scissors == 1 && Aipaper == 1){
+        //win
+        console.log("win");
+        document.getElementById("status").innerHTML = "win";
+        win += 1;
+        document.getElementById('win').innerHTML = win;
+    }else if(scissors == 1 && Aiscissors == 1){
+        //tie
+        console.log("tie");
+        document.getElementById("status").innerHTML = "tie";
+        tie += 1;
+        document.getElementById('tie').innerHTML = tie;
+    }else if(scissors == 1 && Airock == 1){
+        //lose
+        console.log("lose");
+        document.getElementById("status").innerHTML = "lose";
+        lose += 1;
+        document.getElementById('lose').innerHTML = lose;
     }
 }
+
